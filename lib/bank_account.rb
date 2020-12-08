38f88a0791b8f28dffbc3e9fdd8bb4fc7e709922
Ben Spotswood <spotswoodb@gmail.com>
2020-12-07 20:54:19 -0600

    Done.

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,3 +1,46 @@
 class BankAccount
+    attr_reader :name
+    attr_accessor :balance, :status
+    
+    @@all = []
+    def initialize(name)
+        @name = name
+        @balance = 1000
+        @status = "open"
+        @@all << self
+    end
 
+    def deposit(amount)
+        self.balance += amount
+    end
+    
+    def withdrawal(amount)
+        self.balance -= amount
+    end
+
+    def deposit(amount)
+        @balance += amount
+    end
+
+    def display_balance
+        "Your balance is $#{self.balance}."
+    end
+
+    def valid?
+        if @balance <= 0
+            false
+        elsif @status == "closed"
+            false
+        else
+            true
+        end
+    end
+
+    def close_account
+        self.status = "closed"
+    end
+
+    def self.all
+        @@all
+    end
 end
--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,3 +1,61 @@
 class Transfer
-  # your code here
+  attr_accessor :sender, :receiver, :amount, :status
+  
+  def initialize(sender, receiver, amount)
+      @sender = sender
+      @receiver = receiver
+      @amount = amount
+      @status = "pending"
+  end
+
+  # def valid?
+  #   if sender.valid? && receiver.valid?
+  #     true
+  #   else
+  #     false
+  #   end
+  # end
+
+  def valid?
+    @sender.valid? && @receiver.valid?
+  end
+  
+
+  # def execute_transaction
+  #   if !self.valid? && @sender.balance <= @amount
+  #     @status = "rejected" 
+  #     puts "Transaction rejected. Please check your account balance."
+  #   else @status == "pending"
+  #     BankAccount.all.map do |person|
+  #       if person == @sender
+  #         person.balance -= @amount
+  #       elsif person == @receiver
+  #         person.balance += @amount
+  #       end
+  #     end
+  #   end
+  #   @status = "complete"
+  # end
+
+  def execute_transaction
+    if valid? && sender.balance > amount && self.status == "pending"
+      sender.withdrawal(self.amount)
+      receiver.deposit(self.amount)
+      self.status = "complete"
+    else
+      self.status = "rejected"
+      "Transaction rejected. Please check your account balance."
+    end
+  end
+
+  def reverse_transfer
+    if valid? && receiver.balance > amount && self.status == "complete"
+      sender.deposit(self.amount)
+      receiver.withdrawal(self.amount)
+      self.status = "reversed"
+    else
+      self.status = "rejected"
+      "Transaction rejected. Please check your account balance."      
+    end
+  end
 end
